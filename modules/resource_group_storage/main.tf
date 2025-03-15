9f5ac9f5250f87bd1e552b68e431b38c7c7baf41
88victory88 <victory.otrok@gmail.com>
2025-03-15 15:07:31 +0800

    Solution2

--- modules/resource_group_storage/main.tf
+++ modules/resource_group_storage/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_storage_account" "storage" {
   account_replication_type = "LRS"
 }
 module "resource_group_storage" {
-  source              = "YOUR_GITHUB_USERNAME/resource_group_storage/azurerm"
+  source              = "88victory88/terraform-azurerm-resource_group_storage/azurerm"
   version            = "1.0.0"
   resource_group_name = "example-rg"
   location           = "East US"
